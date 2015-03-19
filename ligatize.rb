8fb20893fb0ab1faa5fff5c5f42e3d8e798c3b6c
Caden Lovelace <caden@herostrat.us>
2015-03-19 23:27:02 +0000

    Add files from other directory.

--- ligatize.rb
+++ ligatize.rb
@@ -4,11 +4,11 @@ Bundler.require
 
 require_relative 'lib/font_art/ligature_builder'
 
-ROOT_FOLDER = File.expand_path File.join(File.dirname(__FILE__), '..')
+ROOT_FOLDER = File.expand_path File.join(File.dirname(__FILE__))
 
 FontArt::LigatureBuilder.build(
   stock_name: "DroidSans",
-  stock_file: File.join(ROOT_FOLDER, 'DroidSans.ttf'),
+  stock_file: File.join(ROOT_FOLDER, 'fonts', 'DroidSans.ttf'),
   custom_name: "icomoon",
-  custom_file: File.join(ROOT_FOLDER, 'icomoon.ttf'),
+  custom_file: File.join(ROOT_FOLDER, 'fonts', 'icomoon.ttf'),
   synth_file: File.join(ROOT_FOLDER, 'builds', 'synth.ttf'))
